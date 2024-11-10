c315c3587966cece395d6a59168e002ca278dd60
0Z0SK0 <45397007+0Z0SK0@users.noreply.github.com>
2024-11-10 21:46:55 +0500

    Added methods for get settings key, get map key and decrypt map

--- tggodecrypt.go
+++ tggodecrypt.go
@@ -9,6 +9,78 @@ import (
 	"os"
 )
 
+func GetMapKey(mapPath string, password string) (out []byte) {
+	f, err := os.Open(mapPath)
+	if err != nil {
+		log.Fatalf("could not open file '%s': %+v", mapPath, err)
+	}
+	defer f.Close()
+
+	rawTDF, err := tdata.ReadRawTDF(f)
+	if err != nil {
+		log.Fatalf("could not interpret file '%s': %+v", mapPath, err)
+	}
+
+	eMap, err := encrypted.ReadEMap(rawTDF)
+	if err != nil {
+		log.Fatalf("could not interpret map file: %+v", err)
+	}
+
+	out, err = eMap.GetKey(password)
+	if err != nil {
+		log.Fatalf("could not decrypt map file: %+v", err)
+	}
+
+	return out
+}
+
+func DecryptMapFile(mapPath string, password string) (out []byte) {
+	f, err := os.Open(mapPath)
+	if err != nil {
+		log.Fatalf("could not open file '%s': %+v", mapPath, err)
+	}
+	defer f.Close()
+
+	rawTDF, err := tdata.ReadRawTDF(f)
+	if err != nil {
+		log.Fatalf("could not interpret file '%s': %+v", mapPath, err)
+	}
+
+	eMap, err := encrypted.ReadEMap(rawTDF)
+	if err != nil {
+		log.Fatalf("%+v", err)
+	}
+
+	out, err = eMap.Decrypt(password)
+	if err != nil {
+		log.Fatalf("%+v", err)
+	}
+
+	return out
+}
+
+func GetSettingsKey(settingsPath string, password string) (out []byte) {
+	f, err := os.Open(settingsPath)
+	if err != nil {
+		log.Fatalf("could not open file '%s': %+v", settingsPath, err)
+	}
+	defer f.Close()
+
+	rawTDF, err := tdata.ReadRawTDF(f)
+	if err != nil {
+		log.Fatalf("could not interpret file '%s': %+v", settingsPath, err)
+	}
+
+	settings, err := encrypted.ReadESettings(rawTDF)
+	if err != nil {
+		log.Fatalf("could not interpret settings file: %+v", err)
+	}
+
+	out = settings.GetKey(password)
+
+	return out
+}
+
 func DecryptSettingsFile(settingsPath string, password string) (out []byte) {
 	f, err := os.Open(settingsPath)
 	if err != nil {
