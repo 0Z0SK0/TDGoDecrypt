1f8fe64fadfb1a7648e8ed434ab1a79234ff3b5f
0Z0SK0 <45397007+0Z0SK0@users.noreply.github.com>
2024-11-11 15:12:20 +0500

    Add ExportDecryptedMap method and DecryptedMap struct (+ comments)

--- tggodecrypt.go
+++ tggodecrypt.go
@@ -9,6 +9,8 @@ import (
 	"os"
 )
 
+type DecryptedMap decrypted.DMap
+
 func GetMapKey(mapPath string, password string) (out []byte) {
 	f, err := os.Open(mapPath)
 	if err != nil {
@@ -34,6 +36,11 @@ func GetMapKey(mapPath string, password string) (out []byte) {
 	return out
 }
 
+// DecryptMapFile decrypt map-file normally located in tdata/D877F783D5D3EF8C/map0
+//
+// produces
+//
+// []byte output, that can be used in ExportDecryptedMap
 func DecryptMapFile(mapPath string, password string) (out []byte) {
 	f, err := os.Open(mapPath)
 	if err != nil {
@@ -54,6 +61,22 @@ func DecryptMapFile(mapPath string, password string) (out []byte) {
 	out, err = eMap.Decrypt(password)
 	if err != nil {
 		log.Fatalf("%+v", err)
+	}
+
+	return out
+}
+
+// ExportDecryptedMap converts values obtained from DecryptMapFile to human-readable form
+//
+// produces
+//
+// DecryptedMap struct, with strings keys and uint32 values
+func ExportDecryptedMap(data []byte) (out decrypted.DMap) {
+	var err error
+
+	out, err = decrypted.ReadDMap(data)
+	if err != nil {
+		log.Fatalf("could not decrypt map data: %+v", err)
 	}
 
 	return out
@@ -81,6 +104,11 @@ func GetSettingsKey(settingsPath string, password string) (out []byte) {
 	return out
 }
 
+// DecryptSettingsFile decrypt settings-file normally located in tdata/settingss
+//
+// produces
+//
+// []byte output, that can be converted with json.Marshal
 func DecryptSettingsFile(settingsPath string, password string) (out []byte) {
 	f, err := os.Open(settingsPath)
 	if err != nil {
